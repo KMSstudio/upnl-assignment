709b37a65ce514cdb1be99a2164501e3bb313219
MS Kang <tomskang@naver.com>
2025-06-12 21:20:51 +0900

    feat: PlayerBehavior.cs의 현대화

---
+++ Assets/Scripts/GameScene/BulletBehavior/BulletBehavior.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour {
+    [Header("Bullet Settings")]
+    public float speed = 10f;
+    public float lifetime = 5f;
+    public int damage = 500;
+
+    private Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false; // 중력은 아직 적용 안 함
+        rb.velocity = transform.up * speed;
+        Destroy(gameObject, lifetime);
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Map")) {
+            Destroy(gameObject);
+        }
+    }
+}
